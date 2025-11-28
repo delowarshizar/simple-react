60a6b8d0b48d1b36de16752150b50691c6633161
delowarshizar <husain22205101453@diu.edu.bd>
2025-11-29 00:37:20 +0600

    usestatedone

--- practice-react/src/App.jsx
+++ practice-react/src/App.jsx
@@ -1,11 +1,35 @@
-
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [ini, setIni] = useState(0);
 
+  function Increase() {
+    const neValue = ini + 1;
+    setIni(neValue);
+  }
+  function Decrease() {
+    const neValue = ini - 1;
+    setIni(neValue);
+  }
+  function Reset() {
+    const neValue = 0;
+    setIni(neValue);
+  }
+  const style = {
+    border: "3px solid yellow",
+    padding: "10px",
+    borderRadius: "15px",
+  };
   return (
     <>
       <h1>Practice Task Here: </h1>
+      <div style={style}>
+        <h3>Count: {ini}</h3>
+        <button onClick={Increase}>Increase</button>
+        <button onClick={Decrease}>Decrease</button>
+        <button onClick={Reset}>Reset</button>
+      </div>
     </>
   );
 }
