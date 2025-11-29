74eed986fd75b0b660b17250d3294a62799f4eac
delowarshizar <husain22205101453@diu.edu.bd>
2025-11-30 00:17:27 +0600

    dynamic logicallyy css

--- world-go/src/components/Countries/Countries.jsx
+++ world-go/src/components/Countries/Countries.jsx
@@ -1,15 +1,18 @@
 import React, { use } from "react";
 import Country from "../Country";
+import "./Countries.css";
 
 const Countries = ({ countryData }) => {
   const countriesData = use(countryData);
   const country = countriesData.countries;
   return (
     <div>
-      <h1>My React Project</h1>
-      {country.map((country) => (
-        <Country country={country}></Country>
-      ))}
+      <h1>World All Countries </h1>
+      <div className="countries">
+        {country.map((country) => (
+          <Country key={country.cca3.cca3} country={country}></Country>
+        ))}
+      </div>
     </div>
   );
 };
--- world-go/src/components/Country.jsx
+++ world-go/src/components/Country.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Country.css";
 
 const Country = ({ country }) => {
-  console.log();
+  const [visited, setVisted] = useState(0);
+  const handleVisited = () => {
+    {
+      setVisted(!visited);
+    }
+  };
   return (
-    <div>
+    <div className={`country ${visited && `visited`}`}>
       <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
       <p>Common Name: {country.name.common}</p>
       <p>Official Name: {country.name.official}</p>
+      <p>
+        Area: {country.area.area}
+        {country.area.area > 30000 ? " Big Country" : " Small Country"}
+      </p>
+      <p>Population: {country.population.population}</p>
+      <button onClick={handleVisited}>
+        {visited ? "Visited" : "Not Visited"}
+      </button>
     </div>
   );
 };
